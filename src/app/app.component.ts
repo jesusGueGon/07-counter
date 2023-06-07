63620aa409e79ca3d47424fc0d7c1d8d68a0f295
jesusGueGon <jesus.guerrero@bosonit.com>
2023-06-07 10:46:17 +0200

    funcionando contador completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,93 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { CounterService } from './services/counter.service';
+import { interval, timer, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = '07-counter';
+
+  auxNumero = 0;
+
+  subs: any;
+  idIntervalo: any;
+
+  setTo = 0;
+  step = 0;
+
+  // @ViewChild('steps')
+  // step!: ElementRef<HTMLInputElement>;
+
+  constructor(private counterService: CounterService) { }
+
+  get counter() {
+    console.log(this.step);
+    console.log(this.setTo);
+    return this.counterService.numero;
+
+  }
+
+  onComenzar() {
+
+    this.counterService.isReset = false;
+    const subscription = this.counterService.obs$;
+    this.counterService.step = this.step;
+    console.log(this.subs);
+    this.subs = subscription;
+    subscription.subscribe();
+
+  }
+
+  onPause() {
+
+    this.counterService.isReset = true;
+    this.counterService.onComenzar = false;
+
+  }
+
+
+  onDetener() {
+
+    this.counterService.isReset = true;
+    this.counterService.onComenzar = true;
+    this.counterService.setTo = this.setTo;
+
+  }
+
+  onCountUp() {
+    this.counterService.countDown = false;
+  }
+
+  onCountDown() {
+    this.counterService.countDown = true;
+  }
+
+
+
+
+
+  // subscriber: any;
+
+  ngOnInit(): void {
+
+  }
+
+  setCountTo(setTo: number) {
+    this.setTo = setTo;
+  }
+
+  // onConverirSteps(): number {
+  //   const valorStep = this.step.nativeElement.value;
+  //   let stepsFinal = parseInt(valorStep);
+  //   return stepsFinal;
+  // }
+
+
+
+
+
 }
--- src/app/services/counter.service.ts
+++ src/app/services/counter.service.ts
@@ -1,12 +1,84 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject, interval, map, pipe } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CounterService {
 
+  public numero: number = 0;
+  public setTo: number = 0;
+  public step: number = 0;
+
+  public isReset: boolean = false;
+  public onComenzar: boolean = false;
+  public countDown: boolean = false;
+
   constructor() { }
 
+  public obs$ = new Observable<number>( subs => {
 
+    const intervalId = setInterval( () => {
+
+      if(this.isReset) {
+        subs.complete();
+        clearInterval(intervalId);
+        if(this.onComenzar)
+        {
+          this.numero = this.setTo;
+        }
+
+      }
+      else
+      {
+        if(this.countDown)
+        {
+          if(this.step === 0)
+          {
+            this.numero = this.numero - 1;
+          }
+          else
+          {
+            this.numero = this.numero - this.step;
+          }
+        }
+        else
+        {
+          if(this.step === 0 )
+          {
+            this.numero = this.numero + 1;
+          }
+          else
+          {
+            this.numero = this.numero + this.step
+          // console.log('hola mundo');
+          }
+
+        }
+
+      }
+
+    } ,1000);
+  });
+
+
+  // startCount(cont: number, step: number) {
+
+
+
+  //   interval(1000)
+  //   .pipe(
+  //     map( cont => {
+  //       cont = cont + step;
+  //     }),
+
+  //   )
+  //   .subscribe( val => console.log(val));
+
+  // }
+
+  pauseCount() {
+
+  }
 
 }
